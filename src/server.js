1787b0e50c1fe06e26c7c84f835891b97b4c439e
John Prabu A <johnprabu0702@gmail.com>
2025-01-03 13:06:08 +0530

    gpt server separated from embedding server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.EMBED_PORT || 3000;
+const server_url = process.env.EMBED_SERVER_URL || "http://localhost";
 
 // Middleware
 app.use(express.json({ limit: "10mb" }));
@@ -12,7 +13,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 let modelPipeline;
-let generator;
 
 // Function to load the model
 async function loadModel() {
@@ -20,9 +20,6 @@ async function loadModel() {
     console.log("Loading the Feature Extraction model...");
     modelPipeline = await pipeline("feature-extraction");
     console.log("Model loaded successfully");
-    console.log("Loading the GPT-Neo model...");
-    generator = await pipeline("text-generation", "Xenova/gpt2"); // Replace with your model
-    console.log("Model loaded successfully!");
   } catch (error) {
     console.error("Error loading model:", error);
     process.exit(1); // Exit if the model fails to load
@@ -93,8 +90,15 @@ app.post("/similarity", (req, res) => {
     });
 
     // Sort by similarity in descending order and return only the IDs
+    // .filter((a) => {
+    //   console.log(a.similarity);
+    //   return a.similarity > 0.1;
+    // })
     res.json(
-      output.sort((a, b) => b.similarity - a.similarity).map((item) => item.id)
+      output
+        .filter((a) => a.similarity > 0.2)
+        .sort((a, b) => b.similarity - a.similarity)
+        .map((item) => item.id)
     );
   } catch (error) {
     console.error("Error calculating similarity:", error);
@@ -102,34 +106,9 @@ app.post("/similarity", (req, res) => {
   }
 });
 
-// Endpoint to generate a response
-app.post("/chat", async (req, res) => {
-  const { message } = req.body;
-
-  if (!message) {
-    return res.status(400).json({ error: "Message is required" });
-  }
-
-  try {
-    const output = await generator(message, {
-      max_length: 50,
-      num_return_sequences: 1,
-      temperature: 0.7,
-    });
-
-    const reply = output[0].generated_text;
-    res.json({ reply });
-  } catch (error) {
-    console.error("Error generating response:", error);
-    res.status(500).json({ error: "Failed to generate response" });
-  }
-});
-
 // Start the server
 app.listen(port, () => {
   console.log(
-    `Server is running on ${
-      process.env.SERVER_URL || `http://localhost:${port}`
-    }`
+    `Server is running on ${process.env.SERVER_URL || `${server_url}:${port}`}`
   );
 });
